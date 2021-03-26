0dd1f4c552862cc85d20920d29e7426d9560ee11
“your_github_username” <78240872+sehrishsaeed@users.noreply.github.com>
2021-03-25 20:36:57 -0400

    Feature: Set up calculator with subtract method

--- src/Calculator.py
+++ src/Calculator.py
@@ -2,6 +2,10 @@ def addition(a, b):
     return a + b
 
 
+def subtract(a, b):
+    return a - b
+
+
 class Calculator:
     result = 0
 
@@ -11,5 +15,9 @@ class Calculator:
         pass
 
     def add(self, a, b):
-        self.result = a + b
-        return addition(a, b)
+        self.result = addition(a, b)
+        return self.result
+
+    def subtract(self, a, b):
+        self.result = subtract(a, b)
+        return self.result
